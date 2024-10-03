f8b0d8c957f2ccad7e241a1f4c9e1734ec65bd38
Rajeswaran <rajeswaran149@gmail.com>
2024-10-03 17:56:16 +0530

    update

--- src/app/config.js
+++ src/app/config.js
@@ -1,2 +1,4 @@
-export const  base_url ="http://localhost:5000"
-// export const  base_url =""
+// export const  base_url ="http://localhost:5000";
+export const  base_url ="https://voice-chatbot-server.vercel.app";
+
+
